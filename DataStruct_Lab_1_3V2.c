9705862ca30db3c5968efcf5504fa42338f6eb57
CHAO <149712568+iamc9ju@users.noreply.github.com>
2024-07-01 01:31:59 +0700

    Update DataStruct_Lab_1_3V2.c

--- DataStruct_Lab_1_3V2.c
+++ DataStruct_Lab_1_3V2.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 int gcd(int a, int b) {
-  if (a == 0) {
-    return b;
-  } else {
-    return gcd(b % a, a);
-  }
+    return a == 0 ? b : gcd(b%a,a);
 }
 int main() {
-  int numberOfInput,gcdOfPair=0;
-  scanf("%d", &numberOfInput);
-  int arrInput[numberOfInput];
-  for (int i = 0; i < numberOfInput; i++) {
-    scanf("%d", &arrInput[i]);   
-  }
-  gcdOfPair = gcd(arrInput[0],arrInput[1]);
-  for(int i=2;i<numberOfInput;i++){
-    gcdOfPair = gcd(gcdOfPair,arrInput[i+1]);
-  }
-  printf("%d",gcdOfPair);
-return 0;
+    int n, result;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    result = arr[0];
+    for (int i = 1; i < n; i++) {
+        result = gcd(result, arr[i]);
+    }
+    printf("%d", result);
+    return 0;
 }
